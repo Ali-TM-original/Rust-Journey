8cd259e9da86ac495730d86a73b1d75616e3d71f
Ali™ <mssslf1984@gmail.com>
2022-01-22 21:02:54 +0500

    Nothing much

--- src/ls.rs
+++ src/ls.rs
@@ -9,18 +9,22 @@ pub fn work(){
 fn working(path: &Path){
     let mut total_file  = 0;
     let mut total_size = 0;
+    let mut is_dir:&str;
     if path.is_dir(){
         for file in fs::read_dir(path).unwrap(){
             total_file+=1;
             let file_path = file.unwrap().path();
             let file_size = fs::metadata(&file_path).unwrap().len();
+            if fs::metadata(&file_path).unwrap().is_dir(){is_dir = "<DIR>";}else{is_dir = "<FILE>"}
             let last_modified = fs::metadata(&file_path).unwrap().modified().unwrap().elapsed().unwrap().as_secs();
             let seconds = last_modified % 60;
             let minutes = seconds % 60;
             let hours = minutes / 60;
             total_size += file_size;
-            println!("File:{} Size:{} bytes LastModified:{}h:{}m:{}s", file_path.display(), file_size, hours, minutes, seconds);
+            println!("File:{file_name} {dir} Size:{file_size}bytes LastModified:{hr}h:{min}m:{sec}s",
+            file_name=file_path.display(), file_size=file_size, hr=hours, min=minutes, sec=seconds, dir=is_dir);
+            println!("")
         };
-        println!("{}File(s) {}Bytes", total_file, total_size);
+        println!("{} File(s) {}Bytes", total_file, total_size);
     }
 }
